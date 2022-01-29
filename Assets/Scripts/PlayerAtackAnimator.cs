97378743094226268428d0cdbec6a9aa0ededf64
Gabriel Oliveira do Carmo <gabriel.carmo@smartgo.com.br>
2022-01-29 20:21:11 -0300

    atack

---
+++ Assets/Scripts/PlayerAtackAnimator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAtackAnimator : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("AtackTrigger");
+        }
+    }
+}
